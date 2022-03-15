aa502b355f28af53cafd2e9f10f91e3bd040e3ec
Bruno Martins <bgcngm@gmail.com>
2022-03-15 20:53:47 +0000

    x3: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Idd03d8c9a2bca88e205b7728d6babb28f1008023

--- device.mk
+++ device.mk
@@ -127,6 +127,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Control groups and task profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    
 # Dolby
 PRODUCT_PACKAGES += \
     DolbyManager
