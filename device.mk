acd1098160ff58a7867ace55ac48f269a4870850
Pranav Vashi <neobuddy89@gmail.com>
2022-06-25 02:41:51 +0530

    x3: Enable blur
    * Changes QS transparency into blur effect.
    
    Change-Id: I55eaaaf1fa06d328f05baf580e39fca9a1fe298d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -97,6 +97,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
