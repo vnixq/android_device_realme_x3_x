bd12d52855b24fa9464c082644789361d396655d
Gaurav241 <gaurav.govinda2420@gmail.com>
2022-10-26 19:05:27 +0530

    x3: Build android.hardware.nfc@1.2 to vendor

--- device.mk
+++ device.mk
@@ -284,6 +284,10 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2.vendor \
+    android.hardware.nfc@1.2-service
+
+PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
