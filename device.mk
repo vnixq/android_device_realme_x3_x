c828e81acb73cf4184fcdc0995f246e0f5f95264
LuK1337 <priv.luk@gmail.com>
2022-10-31 19:00:37 +0530

    x3: fingerprint: Update for oplus Android 12 vendor
    [ghostrider-reborn]
    - Squash of:
    - https://github.com/realme-sm8250-devs/android_hardware_oplus/commit/7f4f347c2c926bfb52e166a2b4cd50c7c7f44ea4
    - https://github.com/realme-sm8250-devs/android_hardware_oplus/commit/b336b8146cf84e66eb3a232ccaec29d073eb0b93
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: akmacc <akjagxz@outlook.com>

--- device.mk
+++ device.mk
@@ -174,8 +174,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.x3 \
-    vendor.oplus.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.3-service.x3
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
@@ -416,8 +415,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/oplus
+    $(LOCAL_PATH)
 
 # Sensors
 PRODUCT_PACKAGES += \
