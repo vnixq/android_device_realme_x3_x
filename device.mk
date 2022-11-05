0a4c70bbb323c3e99ed3748bc8801d7ff122e5c5
Retypolkg <retypolkg2021@gmail.com>
2022-11-05 22:21:15 +0100

    x3: Provide android.hardware.power@1.2 on vendor

--- device.mk
+++ device.mk
@@ -161,6 +161,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
 # RCS
