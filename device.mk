8753b39f7abf94a3dea79f8cec616bf503b65c7e
luk1337 <priv.luk@gmail.com>
2023-10-25 23:50:55 +0530

    x3: Copy keylayouts to vendor Change-Id: I887d81bc2312419f7bc0acb475123b3cfdce9506

--- device.mk
+++ device.mk
@@ -104,8 +104,8 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/configs/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/touchpanel.kl
 
 # IRSC
 PRODUCT_COPY_FILES += \
