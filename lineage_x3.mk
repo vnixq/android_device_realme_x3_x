ca14aa081e0e750f0b2b5978e496836636701ef4
Juhyung Park <qkrwngud825@gmail.com>
2023-11-23 00:23:19 +0000

    x3: Stop shipping 32-bit Zygote
    Pixel 7 onwards also do the same, meaning that the user inconvenience is
    none if not minimal.
    
    This saves non-negligible amount of RAM.
    
    Change-Id: Ide8671817d15e79ab61a1665cf3f60e599ff3981
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- lineage_x3.mk
+++ lineage_x3.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit framework configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
