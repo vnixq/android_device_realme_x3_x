84bd0e3e8d88b5751a86b6741bd5c2740e63299b
akmacc <akjagxz@outlook.com>
2024-01-07 18:52:31 +0530

    x3: Initial sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
